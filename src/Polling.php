ad530266f2c0b3a237d01c66fc64a00dcfa88b89
soen <970852318@qq.com>
2020-07-06 18:08:45 +0800

    init

---
+++ src/Polling.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Soen\Delay;
+
+
+class Polling
+{
+    public $duration;
+    function __construct()
+    {
+
+    }
+
+    public function run () {
+
+    }
+
+    public function getOverdueJob () {
+        
+    }
+}
